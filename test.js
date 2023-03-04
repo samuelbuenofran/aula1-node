831405396bc774103c500b1a378b8cd113b4bb2d
Samuel <samuel.buenofran@gmail.com>
2023-03-03 21:47:00 -0300

    Updated files

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 import PromptSync from "prompt-sync";
+import chalk from 'chalk';
 
-const fs = require('fs');
+const print = console.log;
 
 const readLine = new PromptSync();
 
 let text = readLine("Enter your question: ");
 
-alert(text);
+print(chalk.blue(text));
+
+print(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Hey look buddy, we're going to ask you a few questions about your project.
+
+// Boohoo, this is the end of the program. Let the user know.
+
+/* 
+Receive 5 values;
+Calculate the average of the values;
+n1 + n1 + n3 / n
+
+
+*/
